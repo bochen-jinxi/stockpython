2bf10813ec2a86237a123c1c265be8e1854530d9
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2024-01-06 18:37:11 +0800

    5
    5

--- SQL/top3autoken/writeblockfile要素好图.sql
+++ SQL/top3autoken/writeblockfile要素好图.sql
@@ -1,25 +1,21 @@
-  --æŸ¥è¯¢æŽ’åº
+  --²éÑ¯ÅÅÐò
  --SELECT * FROM  dbo.T900
- --WHERE ciriqi='2022-04-13'
+ --WHERE zhuriqi='2022-04-13'
  --ORDER BY code desc
 
  
  
-SELECT  DISTINCT   CONCAT('exec master.dbo.xp_cmdshell ''echo '+CONVERT(varchar(5), kaishiriqi, 10)+'-'+CONVERT(varchar(5), kaishiriqi, 10)+'                                       '+CONVERT(varchar(5), jieshuriqi, 10)+'-'+CONVERT(varchar(5), jieshuriqi, 10)+'                                                         >>C:\zd_zsone\T0002\blocknew\blocknew.cfg"''',';')
+SELECT  DISTINCT   CONCAT('exec master.dbo.xp_cmdshell ''echo '+CONVERT(varchar(5), kaishiriqi, 10)+'-'+CONVERT(varchar(5), jieshuriqi, 10)+'                                       '+CONVERT(varchar(5), kaishiriqi, 10)+'-'+CONVERT(varchar(5), jieshuriqi, 10)+'                                                         >>C:\zd_zsone\T0002\blocknew\blocknew.cfg"''',';')
   ,  kaishiriqi,
    CONVERT(varchar(5), kaishiriqi, 10)
   FROM [stock].[dbo].[T10000]
   ORDER BY kaishiriqi DESC   
    
-
- 
-
- 
-  SELECT     CONCAT('exec master.dbo.xp_cmdshell ''echo '+REPLACE(REPLACE(code,'sh.',1),'sz.',0)+'   >>C:\zd_zsone\T0002\blocknew\"'+ CONVERT(varchar(5), kaishiriqi, 10)+'-'+ CONVERT(varchar(5), jieshuriqi, 10)+'                                      .blk"''',';')
+SELECT  CONCAT('exec master.dbo.xp_cmdshell ''echo '+REPLACE(REPLACE(code,'sh.',1),'sz.',0)+'   >>C:\zd_zsone\T0002\blocknew\"'+ CONVERT(varchar(5), kaishiriqi, 10)+'-'+ CONVERT(varchar(5), jieshuriqi, 10)+'                                      .blk"''',';')
   ,code,
   REPLACE(REPLACE(code,'sh.',1),'sz.',0),
   kaishiriqi,
    CONVERT(varchar(5), kaishiriqi, 10)
   FROM [stock].[dbo].[T10000] 
-  ORDER BY kaishiriqi desc 
- OFFSET (1-1)*100 ROWS FETCH NEXT 100 ROWS ONLY;
+  ORDER BY kaishiriqi desc  
+  OFFSET (1-1)*100 ROWS FETCH NEXT 100 ROWS ONLY;
