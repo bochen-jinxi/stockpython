74f826d218985a6952cb572da16978f3ba312c8c
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2025-09-23 11:12:15 +0800

    1
    1

---
+++ SQL/top3autoken/存储过程吞噬hhkj.sql
@@ -0,0 +1,174 @@
+USE stock;
+GO
+
+IF OBJECT_ID('sphhkj', 'P') IS NOT NULL
+    DROP PROCEDURE sphhkj;
+GO
+
+CREATE PROCEDURE dbo.sphhkj
+(
+    @StartDate DATE = NULL,      -- 开始日期，可选
+    @EndDate   DATE = NULL,      -- 结束日期，可选
+    @Days      INT  = 60         -- 默认最近60天
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+   
+    -- 默认值: 最近@Days天
+    IF @EndDate IS NULL
+        SET @EndDate = CAST(GETDATE() AS DATE);
+
+    IF @StartDate IS NULL
+        SET @StartDate = DATEADD(DAY, -@Days, @EndDate);
+
+    PRINT '开始日期: ' + CONVERT(VARCHAR(10), @StartDate, 120);
+    PRINT '结束日期: ' + CONVERT(VARCHAR(10), @EndDate, 120);
+
+    -- 清理旧的T90表
+    IF OBJECT_ID('dbo.T90', 'U') IS NOT NULL
+        DROP TABLE dbo.T90;
+
+    -- 生成T90
+    SELECT ROW_NUMBER() OVER(PARTITION BY code ORDER BY riqi DESC) AS riqihao, *
+    INTO dbo.T90
+    FROM lishijiager
+--华宏科技
+--WHERE riqi>='2021-01-14' AND riqi<='2021-02-09' AND code='sz.002645' 
+    WHERE riqi >= @StartDate AND riqi <= @EndDate;
+
+    ;WITH T AS (
+        SELECT 
+            riqihao,
+            (shou - kai) / kai * 100 AS shitifudu,
+            code, riqi, kai, shou, di, gao, chengjiaoliang, pctChg,
+            IIF(shou >= kai, shou, kai) AS maxval,
+            IIF(shou <= kai, shou, kai) AS minval
+        FROM #T90
+        WHERE riqihao <= 19
+    )
+    ,T3 AS (
+        SELECT 
+            ROW_NUMBER() OVER (PARTITION BY code ORDER BY shitifudu DESC) AS RowID,
+            *,
+            (gao/maxVal-1)*100 AS shangyingxianfudu,
+            (minval/di-1)*100 AS xiayingxianfudu
+        FROM T
+    )
+    ,T401 AS (
+        SELECT T.*,
+            CASE
+                WHEN T.kai > A.shou THEN T.kai/A.shou - 1
+                WHEN T.kai < A.shou THEN 1 - A.shou/T.kai
+                ELSE 0
+            END AS val
+        FROM T INNER JOIN T AS A 
+            ON T.code = A.code AND T.riqihao + 1 = A.riqihao
+    )
+    ,T402 AS (
+        SELECT ROW_NUMBER() OVER (PARTITION BY code ORDER BY riqihao DESC) AS RowID, *
+        FROM T401 
+        WHERE val > 0
+    )
+    ,T403 AS (
+        SELECT * 
+        FROM T402
+        WHERE RowID = 1 AND riqihao >= 18-2 AND val > 0.002
+    )
+    ,T4 AS (SELECT * FROM T403)
+    ,T499 AS (
+        SELECT 
+            COUNT(1) OVER (PARTITION BY T3.code) AS zhangdiezhouqishu,
+            T3.pctChg,
+            T4.di AS kaishidi, T4.gao AS kaishigao,
+            T4.code, T4.riqi AS kaishiriqi,
+            T3.riqi,
+            MAX(T3.riqi) OVER (PARTITION BY T3.code) AS jieshuriqi,
+            T3.riqihao, T3.shitifudu, 
+            T4.riqihao AS zuigaojiariqihao, T3.riqihao AS zuihouriqihao,
+            T3.di, T3.kai, T3.shou, T3.gao,
+            COUNT(CASE WHEN T3.shangyingxianfudu = 0 THEN 1 END) OVER(PARTITION BY T3.code) AS wushangyingxianfudushu,
+            COUNT(CASE WHEN T3.xiayingxianfudu = 0 THEN 1 END) OVER(PARTITION BY T3.code) AS wuxiayingxianfudushu,
+            COUNT(CASE WHEN T3.pctChg >= 0 THEN 1 END) OVER(PARTITION BY T3.code) AS yangxianshu,
+            COUNT(CASE WHEN T3.pctChg < 0 THEN 1 END) OVER(PARTITION BY T3.code) AS yinxianshu,
+            MIN(T3.pctChg) OVER (PARTITION BY T3.code) AS zuidadiehuozezuixiaozhang,
+            MAX(T3.gao) OVER (PARTITION BY T3.code) AS zuidagao,
+            MIN(T3.di) OVER (PARTITION BY T3.code) AS zuixiaodi,
+            MAX(T3.shou) OVER (PARTITION BY T3.code) AS zuidashou,
+            MAX(T3.shangyingxianfudu) OVER (PARTITION BY T3.code) AS zuidashangyingxianfudu,
+            MAX(T3.xiayingxianfudu) OVER (PARTITION BY T3.code) AS zuidaxiayingxianfudu,
+            T3.shangyingxianfudu, T3.xiayingxianfudu
+        FROM T4 
+        INNER JOIN T3 ON T4.code = T3.code AND T4.riqihao > T3.riqihao
+        WHERE T4.RowID = 1
+    )
+    ,T6 AS (
+        SELECT ROW_NUMBER() OVER (PARTITION BY code ORDER BY riqi) AS riqihaoasc, *
+        FROM T499
+    )
+    ,T7 AS (
+        SELECT riqihaoasc - ROW_NUMBER() OVER (PARTITION BY code ORDER BY riqi) AS lianxuxiadieriqizu, *
+        FROM T6
+        WHERE pctChg > 0
+    )
+    ,T8 AS (
+        SELECT COUNT(1) OVER (PARTITION BY code, lianxuxiadieriqizu) AS lianxuxiadieshu, *
+        FROM T7
+    )
+    ,T9 AS (
+        SELECT MAX(lianxuxiadieshu) OVER (PARTITION BY code) AS zuidalianxushangzhangshu, *
+        FROM T8
+    )
+    ,T10 AS (
+        SELECT * FROM T9 WHERE zuidalianxushangzhangshu >= 3
+    )
+    ,T5 AS (
+        SELECT *
+        FROM T10
+        WHERE zuidagao/kaishigao - 1 < 0.38 
+          AND zuixiaodi/kaishidi - 1 < 0.01
+          AND zuidashangyingxianfudu < 8 
+          AND zuidaxiayingxianfudu < 4
+    )
+    ,T590 AS (
+        SELECT COUNT(1) OVER (PARTITION BY T5.code) AS suoyoumanzu, *
+        FROM T5
+        WHERE (shangyingxianfudu <= 4 AND xiayingxianfudu <= 1)
+           OR (shangyingxianfudu = 0 OR xiayingxianfudu = 0)
+    )
+    ,T501 AS (
+        SELECT DISTINCT code, kaishiriqi, jieshuriqi, yangxianshu, yinxianshu
+        FROM T590
+        WHERE yangxianshu > yinxianshu
+    )
+    ,T502 AS (
+        SELECT T401.*, kaishiriqi
+        FROM T401 
+        INNER JOIN T501 ON T401.code = T501.code AND T401.riqi = T501.kaishiriqi
+    )
+    ,T503 AS (
+        SELECT T401.*, jieshuriqi
+        FROM T401 
+        INNER JOIN T501 ON T401.code = T501.code AND T401.riqi = T501.jieshuriqi
+    )
+    ,T599 AS (
+        SELECT A.kaishiriqi, B.jieshuriqi, A.code, B.val
+        FROM T502 AS A
+        INNER JOIN T503 AS B ON A.code = B.code
+        INNER JOIN T AS C ON C.code = B.code AND B.riqihao + 1 = C.riqihao
+        WHERE B.pctChg > 0 
+          AND A.shou < B.shou  
+          AND A.di < B.di 
+          AND B.val < 0 
+          AND (
+                (C.pctChg < 0 AND B.shou > C.maxval AND B.kai < C.shou)
+             OR (C.pctChg > 0 AND B.shou > C.maxval AND B.kai < C.kai)
+              )
+    )
+    INSERT INTO T10002 ([kaishiriqi], [jieshuriqi], [code])
+    SELECT DISTINCT kaishiriqi, jieshuriqi, code
+    FROM T599;
+
+END;
+GO
