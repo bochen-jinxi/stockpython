b8c663049b06ad9b186defa0aad08bd8afa6e20f
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2025-09-25 17:13:45 +0800

    1
    1

--- jiage/bat.bat
+++ jiage/bat.bat
@@ -5,6 +5,10 @@ cd /d F:\stockpython\pyrun
 for %%f in (*.py) do (
     echo 正在运行 %%f ...
     python "%%f"
+    
+    :: 等待3分钟（180秒）
+    echo 等待3分钟后执行下一个脚本...
+    timeout /t 180 /nobreak
 )
 
 echo 全部执行完成！
