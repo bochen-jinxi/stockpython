bc02f2e7f5ae9a79071ef1e51dcb6a431e30118a
bochen-jinxi <60917379+bochen-jinxi@users.noreply.github.com>
2025-09-27 12:31:47 +0800

    1
    1

---
+++ SQL/hsbg/writeblockfile要素好图.sql
@@ -0,0 +1,25 @@
+  --查询排序
+ --SELECT * FROM  dbo.T900
+ --WHERE ciriqi='2022-04-13'
+ --ORDER BY code desc
+
+ 
+ 
+SELECT  DISTINCT   CONCAT('exec master.dbo.xp_cmdshell ''echo '+CONVERT(varchar(5), kaishiriqi, 10)+'-'+CONVERT(varchar(5), jieshuriqi, 10)+'                                       '+CONVERT(varchar(5), kaishiriqi, 10)+'-'+CONVERT(varchar(5), jieshuriqi, 10)+'                                                         >>C:\zd_zsone\T0002\blocknew\blocknew.cfg"''',';')
+  ,  kaishiriqi,
+   CONVERT(varchar(5), kaishiriqi, 10)
+  FROM [stock].[dbo].[T10000]
+  ORDER BY kaishiriqi DESC   
+   
+
+ 
+
+ 
+  SELECT  DISTINCT   CONCAT('exec master.dbo.xp_cmdshell ''echo '+IIF(LEN(code)=5,'71#'+REPLACE(REPLACE(code,'sh.',1),'sz.',0),REPLACE(REPLACE(code,'sh.',1),'sz.',0))+'   >>C:\zd_zsone\T0002\blocknew\"'+ CONVERT(varchar(5), kaishiriqi, 10)+'-'+ CONVERT(varchar(5), jieshuriqi, 10)+'                                      .blk"''',';')
+  ,code,
+ IIF(LEN(code)=5,'71#'+REPLACE(REPLACE(code,'sh.',1),'sz.',0),REPLACE(REPLACE(code,'sh.',1),'sz.',0)),
+  kaishiriqi,
+   CONVERT(varchar(5), kaishiriqi, 10)
+  FROM [stock].[dbo].[T10000] 
+  ORDER BY kaishiriqi desc 
+-- OFFSET (1-1)*100 ROWS FETCH NEXT 100 ROWS ONLY;
